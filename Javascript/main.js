a24cb9759fec58e6306a7fcc672f532019496ecd
chebet24 <chebetkiturm@gmail.com>
2024-06-04 11:31:32 +0300

    introduction to Javascript

---
+++ Javascript/main.js
@@ -0,0 +1,8 @@
+//just the basics of the js
+// creating a storage space 
+var height,width,total;
+height = 180;
+width = 60;
+total = height * width ;
+
+document.write("The multiplication of height and width is ",total);
